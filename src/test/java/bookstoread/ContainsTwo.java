7a85cb272c4f0eb105d5092ac5fddb8612e1fb1a
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-07-24 13:09:02 +0100

    Exception Testing.

--- src/test/java/bookstoread/ContainsTwo.java
+++ src/test/java/bookstoread/ContainsTwo.java
@@ -1,18 +1,27 @@
 package bookstoread;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContainsTwo {
     @Test
     public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
         BookShelf shelf = new BookShelf();
-        List<String> books = shelf.books();
-        shelf.add("Effective Java");
-        shelf.add("Code Complete");
+        shelf.add("Effective Java", "Code Complete");
 
         List<String> books2 = shelf.books();
         assertEquals(2, books2.size(), () -> "Bookshelf should have two books");
+    }
+
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks(){
+        BookShelf shelf = new BookShelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "Please, the books ought to be empty");
     }
 }
