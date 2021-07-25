8175134fbffb9a0a61afa0375794a7adc7de9ac3
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-07-25 09:44:17 +0100

    Extensive code refactoring.

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("<= Bookshelf Specification =>")
 public class BookShelfSpec {
@@ -16,4 +15,28 @@ public class BookShelfSpec {
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty");
     }
+
+    @Test
+    public void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java", "Code Complete");
+
+        List<String> books2 = shelf.books();
+        try {
+            books2.add("Spring framework reference.");
+            fail(()-> "Should not be able to add book to books");
+        }catch (Exception e){
+            assertTrue( e instanceof UnsupportedOperationException,()-> "Should throw UnsupportedOperationException.");
+        }
+        assertEquals(2, books2.size(), () -> "Bookshelf should have two books");
+    }
+
+    @Test
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks(){
+        BookShelf shelf = new BookShelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "Please, the books ought to be empty");
+    }
 }
+
